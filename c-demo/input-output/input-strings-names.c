077bcc0b0e65fef4dcd158fabb5092d5e08e3388
amah-nnachi-lofty <loftdot@gmail.com>
2023-02-24 10:41:20 +0100

    Input and out program to read a line of string

--- c-demo/input-output/input-strings-names.c
+++ c-demo/input-output/input-strings-names.c
@@ -6,10 +6,11 @@ int main(void)
   int age;
 
   printf("Enter yor name: ");
-  scanf("%s", name);
+  fgets(name, sizeof(name), stdin); // fgets(name, sizeof(name), stdlin); // fgets function read string
   printf("Enter your age: ");
   scanf("%d", &age);
   printf("My name is: %s and my age is:  %d\n", name, age);
+
   getchar();
   return(0);
-}
+  }
