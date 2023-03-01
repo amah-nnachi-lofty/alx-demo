e46e093c1bae8ac1f6271b9911886ea342ee12f0
amah-nnachi-lofty <loftdot@gmail.com>
2023-03-01 13:02:51 +0100

    Program to print the address of memory location

--- c-demo/arrays.c
+++ c-demo/arrays.c
@@ -5,7 +5,7 @@ int main()
 {
 	int arr[5] = {2,4,6,7,10};
 
-	printf("%p\n", &(arr[0]));
+	printf("The memory address is: %p\n", &(arr[0]));
 	printf("%p\n", &(arr[1]));
 	printf("%p\n", &(arr[2]));
 	printf("%p\n", &(arr[3]));
