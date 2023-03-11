d38143bc56eaead43c1b352fdbc1b954c567d136
amah-nnachi-lofty <loftdot@gmail.com>
2023-03-11 20:40:40 +0100

    program pointer

---
+++ c-demo/pointersx/pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+	int x; // normal variable declaration of type int
+	int *ptr; // declaration of a pointer => pointer is an address of a variable
+	x = 5;  // assign value of 5 to x
+	ptr = &x; // prints address of x
+
+	printf("x is : %d\n", x); // print the value of x
+	printf("The memory address of x is: %p\n", ptr); // print the memory address where x is located
+
+	// Pointer => get the variable of variable memory address
+	printf("Value at the ponter address is: %d\n", *ptr); // vlaue at the address of ptr(&x)
+	return 0;
+}
